84d33c6fea5709c7bdf2cf1980432ddfff81a037
Mohamed.Ramadan <mohamed.a.ramadan0091@gmail.com>
2023-01-06 22:42:54 +0200

    Add Lazy Load Function

--- js/main.js
+++ js/main.js
@@ -4,20 +4,18 @@ let links = document.querySelectorAll("header nav ul li");
 //Put class "sticky" to header when scroll
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0); // New Rule for me
-  console.log(window.scrollY);
 });
 let aboutMe = document.querySelector(".about-me");
 let myName = document.querySelector("footer span");
 myName.onclick = function (event) {
   aboutMe.classList.toggle("show");
 };
-/*
-links.forEach(function (li) {
-  li.addEventListener("click", function (event) {
-    links.forEach(function (element) {
-      element.classList.remove("active-link");
-    });
-    event.currentTarget.classList.toggle("active-link");
-  });
-});
-*/
+/* =============== ADD Lazy Load Function ===============*/
+lozad(".lozad", {
+  load: function (el) {
+    el.src = el.dataset.src;
+    el.onload = function () {
+      el.classList.add("fade");
+    };
+  },
+}).observe();
